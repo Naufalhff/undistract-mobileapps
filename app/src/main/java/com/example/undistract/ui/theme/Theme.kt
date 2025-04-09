de60542fd6f38bd5a6648ff89347efe7e4a7b92b
Firlyansyah <undeadcatgirl5@gmail.com>
2025-04-09 10:45:31 +0700

    fix(theme) Fix error typography

--- app/src/main/java/com/example/undistract/ui/theme/Theme.kt
+++ app/src/main/java/com/example/undistract/ui/theme/Theme.kt
@@ -7,9 +7,12 @@ import androidx.compose.material3.darkColorScheme
 import androidx.compose.material3.dynamicDarkColorScheme
 import androidx.compose.material3.dynamicLightColorScheme
 import androidx.compose.material3.lightColorScheme
+import androidx.compose.material3.Typography
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+
+val Typography = Typography()
 
 private val DarkColorScheme = darkColorScheme(
     primary = Purple80,
