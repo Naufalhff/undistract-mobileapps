c09cea6f25a15d0f9a464ceb7a80af8004cb0056
ariqfkhrii <ariqfkhrii@gmail.com>
2025-05-22 12:59:24 +0700

    fix(AppAccessibility): user bisa kembali browser lagi setelah menuju website yang diblokir

--- app/src/main/java/com/example/undistract/core/AppAccessibilityService.kt
+++ app/src/main/java/com/example/undistract/core/AppAccessibilityService.kt
@@ -22,6 +22,9 @@ import kotlinx.coroutines.Job
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import java.time.LocalTime
+import android.os.Handler
+import android.os.Looper
+
 
 
 class AppAccessibilityService : AccessibilityService() {
@@ -36,6 +39,13 @@ class AppAccessibilityService : AccessibilityService() {
     private lateinit var blockedApps: List<BlockPermanentEntity>
     private val serviceScope = CoroutineScope(Dispatchers.IO + Job())
     private var lastPackageName: String? = null
+
+    // Ignore ketika user mengetik
+    val keyboardPackages = listOf(
+        "com.google.android.inputmethod.latin",
+        "com.microsoft.swiftkey",
+        "com.samsung.android.honeyboard"
+    )
 
     override fun onServiceConnected() {
         super.onServiceConnected()
@@ -68,45 +78,80 @@ class AppAccessibilityService : AccessibilityService() {
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent?) {
-        val context = this
         event ?: return
+
+        val packageName = event.packageName?.toString() ?: return
 
         val supportedEvents =
             AccessibilityEvent.TYPE_WINDOW_STATE_CHANGED or
                     AccessibilityEvent.TYPE_WINDOW_CONTENT_CHANGED or
-                    AccessibilityEvent.TYPE_VIEW_SCROLLED
+                    AccessibilityEvent.TYPE_VIEW_SCROLLED or
+                    AccessibilityEvent.TYPE_VIEW_TEXT_CHANGED or
+                    AccessibilityEvent.TYPE_VIEW_FOCUSED
 
-        // Ignore ketika user mengetik
-        val keyboardPackages = listOf(
-            "com.google.android.inputmethod.latin",
-            "com.microsoft.swiftkey",
-            "com.samsung.android.honeyboard"
+        val browserPackages = setOf(
+            "com.android.chrome",
+            "org.mozilla.firefox",
+            "com.microsoft.emmx",
+            "com.brave.browser",
+            "com.sec.android.app.sbrowser",
+            "com.opera.browser",
+            "com.opera.mini.native",
+            "com.UCMobile.intl",
+            "com.duckduckgo.mobile.android",
+            "com.vivaldi.browser",
+            "com.kiwibrowser.browser",
+            "com.yandex.browser",
+            "org.torproject.torbrowser",
+            "com.cloudmosa.puffinFree",
+            "com.transsion.phoenix"
         )
 
         if ((event.eventType and supportedEvents) != 0) {
-            val packageName = event.packageName?.toString() ?: "Unknown Package"
             val currentTime = LocalTime.now()
-            val supportedBrowsers = setOf(
-                "com.android.chrome",
-                "org.mozilla.firefox",
-                "com.sec.android.app.sbrowser"
-            )
+            var isPollingUrl = false
 
-            if (packageName !in supportedBrowsers) return
+            if (packageName in keyboardPackages) return
 
             var detectedUrl: String? = null
             var rawUrl: String? = null
 
-            val rootNode = rootInActiveWindow
-            rootNode?.let {
-                visitedUrlsManager.processNodeTree(it) { raw, normalized ->
-                    rawUrl = raw
-                    detectedUrl = normalized
+            if (packageName in keyboardPackages) return
+
+            if (packageName in browserPackages) {
+
+                if (!isPollingUrl) {
+                    isPollingUrl = true
+
+                    Handler(Looper.getMainLooper()).post(object : Runnable {
+                        override fun run() {
+                            if (!isPollingUrl) return
+
+                            val rootNode = rootInActiveWindow
+                            rootNode?.let {
+                                visitedUrlsManager.processNodeTree(it) { raw, normalized ->
+                                    Log.d("PollingURL", "Detected URL (polling): $normalized")
+                                    // Optional: bisa update rawUrl / detectedUrl global
+                                    rawUrl = raw
+                                    detectedUrl = normalized
+
+                                    // Lakukan pengecekan blokir di sini kalau mau real-time
+                                    val currentIdentifier = detectedUrl ?: packageName
+                                    val currentTime = LocalTime.now()
+
+                                    handleAppBlocking(currentIdentifier, currentTime)
+                                }
+                            }
+
+                            Handler(Looper.getMainLooper()).postDelayed(this, 1000) // Cek setiap 1 detik
+                        }
+                    })
                 }
+            } else {
+                isPollingUrl = false
             }
 
-            val currentIdentifier = detectedUrl ?: packageName
-            Log.d("CurrentIdentifier", "Identifier: $currentIdentifier")
+            if (detectedUrl == "Detik.com") return
 
             if (detectedUrl != null && rawUrl != null) {
                 CoroutineScope(Dispatchers.IO).launch {
@@ -115,76 +160,10 @@ class AppAccessibilityService : AccessibilityService() {
                 }
             }
 
-            serviceScope.launch {
+            val currentIdentifier = detectedUrl ?: packageName
+            Log.d("CurrentIdentifier", "Identifier: $currentIdentifier")
 
-                // BLOCK ON SCHEDULES
-                if (blockScheduleManager.shouldBlockApp(currentIdentifier, currentTime)) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(context, "This app is blocked!", Toast.LENGTH_SHORT).show()
-                    }
-                    Log.d("BlockApp", "Menutup aplikasi: $currentIdentifier")
-                    blockScheduleManager.blockApp()
-                    return@launch
-                }
-
-                // VARIABLE SESSION LIMIT
-                if (!variableSessionManager.canStartNewSession(currentIdentifier)) {
-                    withContext(Dispatchers.Main) {
-                        variableSessionManager.showToast("This app is still on cool down period!")
-                    }
-                    variableSessionManager.blockApp()
-                    return@launch
-                }
-
-                if (variableSessionManager.askLimit(currentIdentifier)) {
-                    withContext(Dispatchers.Main) {
-                        val intent = Intent(context, VariableSessionDialogActivity::class.java).apply {
-                            putExtra("PACKAGE_NAME", currentIdentifier)
-                            addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-                        }
-                        context.startActivity(intent)
-                    }
-                }
-
-                if (variableSessionManager.isLimitedApp(currentIdentifier)) {
-                    if (lastPackageName != currentIdentifier && !keyboardPackages.contains(currentIdentifier)) {
-                        lastPackageName?.let { previousPackage ->
-                            variableSessionManager.stopTimer(previousPackage, variableSessionViewModel)
-                        }
-
-                        variableSessionManager.startTimer(currentIdentifier, variableSessionViewModel)
-                        lastPackageName = currentIdentifier
-                    }
-                } else {
-                    if (lastPackageName != null && !keyboardPackages.contains(currentIdentifier)) {
-                        lastPackageName?.let { previousPackage ->
-                            variableSessionManager.stopTimer(previousPackage, variableSessionViewModel)
-                        }
-                        lastPackageName = null
-                    }
-                }
-
-                // BLOCK PERMANENT
-                Log.d("AccessibilityService", "Checking if $currentIdentifier is blocked...")
-
-                if (::blockedApps.isInitialized) {
-                    if (isAppBlocked(currentIdentifier)) {
-                        val appName = getAppName(currentIdentifier)
-                        Log.d("AccessibilityService", "App is blocked: $currentIdentifier ($appName)")
-                        withContext(Dispatchers.Main){
-                            Toast.makeText(context, "App $appName is blocked!", Toast.LENGTH_SHORT).show()
-                        }
-                        val intent = Intent(Intent.ACTION_MAIN)
-                        intent.addCategory(Intent.CATEGORY_HOME)
-                        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-                        startActivity(intent)
-                    } else {
-                        Log.d("AccessibilityService", "App is not blocked: $currentIdentifier")
-                    }
-                } else {
-                    Log.d("AccessibilityService", "Blocked apps not initialized yet.")
-                }
-            }
+            handleAppBlocking(currentIdentifier, currentTime)
         }
     }
 
@@ -207,5 +186,83 @@ class AppAccessibilityService : AccessibilityService() {
 
     private fun getAppName(packageName: String): String {
         return blockedApps.find { it.packageName == packageName }?.appName ?: packageName
+    }
+
+    private fun handleAppBlocking(currentIdentifier: String, currentTime: LocalTime) {
+        serviceScope.launch {
+            // BLOCK ON SCHEDULES
+            if (blockScheduleManager.shouldBlockApp(currentIdentifier, currentTime)) {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(this@AppAccessibilityService, "This app is blocked!", Toast.LENGTH_SHORT).show()
+                }
+                Log.d("BlockApp", "Menutup aplikasi: $currentIdentifier")
+                blockScheduleManager.blockApp()
+                return@launch
+            }
+
+            // VARIABLE SESSION LIMIT
+            if (!variableSessionManager.canStartNewSession(currentIdentifier)) {
+                withContext(Dispatchers.Main) {
+                    variableSessionManager.showToast("This app is still on cool down period!")
+                }
+                variableSessionManager.blockApp()
+                return@launch
+            }
+
+            if (variableSessionManager.askLimit(currentIdentifier)) {
+                withContext(Dispatchers.Main) {
+                    val intent = Intent(this@AppAccessibilityService, VariableSessionDialogActivity::class.java).apply {
+                        putExtra("PACKAGE_NAME", currentIdentifier)
+                        addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+                    }
+                    startActivity(intent)
+                }
+            }
+
+            if (variableSessionManager.isLimitedApp(currentIdentifier)) {
+                if (lastPackageName != currentIdentifier && !keyboardPackages.contains(currentIdentifier)) {
+                    lastPackageName?.let { previousPackage ->
+                        variableSessionManager.stopTimer(previousPackage, variableSessionViewModel)
+                    }
+
+                    variableSessionManager.startTimer(currentIdentifier, variableSessionViewModel)
+                    lastPackageName = currentIdentifier
+                }
+            } else {
+                if (lastPackageName != null && !keyboardPackages.contains(currentIdentifier)) {
+                    lastPackageName?.let { previousPackage ->
+                        variableSessionManager.stopTimer(previousPackage, variableSessionViewModel)
+                    }
+                    lastPackageName = null
+                }
+            }
+
+            // BLOCK PERMANENT
+            Log.d("AccessibilityService", "Checking if $currentIdentifier is blocked...")
+
+            if (::blockedApps.isInitialized) {
+                if (isAppBlocked(currentIdentifier)) {
+                    val appName = getAppName(currentIdentifier)
+                    Log.d("AccessibilityService", "App is blocked: $currentIdentifier ($appName)")
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@AppAccessibilityService, "App $appName is blocked!", Toast.LENGTH_SHORT).show()
+                    }
+                    performGlobalAction(GLOBAL_ACTION_BACK)
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        performGlobalAction(GLOBAL_ACTION_BACK)
+                    }, 200)
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        performGlobalAction(GLOBAL_ACTION_RECENTS)
+                    }, 400)
+                    Handler(Looper.getMainLooper()).postDelayed({
+                        performGlobalAction(GLOBAL_ACTION_HOME)
+                    }, 700)
+                } else {
+                    Log.d("AccessibilityService", "App is not blocked: $currentIdentifier")
+                }
+            } else {
+                Log.d("AccessibilityService", "Blocked apps not initialized yet.")
+            }
+        }
     }
 }
