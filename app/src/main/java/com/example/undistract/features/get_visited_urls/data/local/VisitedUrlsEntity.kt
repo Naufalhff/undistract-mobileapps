3e62724a0be72500eab068d070b478ed800bc320
ariqfkhrii <ariqfkhrii@gmail.com>
2025-05-29 18:32:50 +0700

    fix(getVisitedUrls): menghilangkan warning

--- app/src/main/java/com/example/undistract/features/get_visited_urls/data/local/VisitedUrlsEntity.kt
+++ app/src/main/java/com/example/undistract/features/get_visited_urls/data/local/VisitedUrlsEntity.kt
@@ -9,7 +9,7 @@ import androidx.room.PrimaryKey
     tableName = "visited_urls",
     indices = [Index(value = ["url"], unique = true)]
 )
-data class VisitedUrlsEntity(
+class VisitedUrlsEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
 
@@ -18,4 +18,25 @@ data class VisitedUrlsEntity(
 
     @ColumnInfo(name = "favicon")
     val favicon: ByteArray? = null,
-)
+) {
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (other !is VisitedUrlsEntity) return false
+
+        if (id != other.id) return false
+        if (url != other.url) return false
+        if (favicon != null) {
+            if (other.favicon == null) return false
+            if (!favicon.contentEquals(other.favicon)) return false
+        } else if (other.favicon != null) return false
+
+        return true
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + url.hashCode()
+        result = 31 * result + (favicon?.contentHashCode() ?: 0)
+        return result
+    }
+}
